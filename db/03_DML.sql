52713a92f3293655f2564f46ca8440ec60c19685
aranparrk <aranparrk@gmail.com>
2026-09-02 13:36:55 +0900

    실습문제

--- db/03_DML.sql
+++ db/03_DML.sql
@@ -35,6 +35,35 @@ CREATE TABLE test_info2 (
 
 INSERT INTO test_info2 VALUES (NULL, '원이', 20);
 INSERT INTO test_info2 (test_name, test_age) VALUES ('리브', 20);
+INSERT INTO test_info2 VALUES (NULL, '메이', 19);
+INSERT INTO test_info2 VALUES (NULL, '미나미', 19);
+INSERT INTO test_info2 VALUES (NULL, '제나', 18);
+INSERT INTO test_info2 (test_name, test_age) VALUES ('박지훈', 24);
+INSERT INTO test_info2 (test_name, test_age) VALUES ('장동윤', 18);
+INSERT INTO test_info2 (test_name, test_age) VALUES ('최우식', 20);
 
 SELECT * FROM test_info2;
 
+-- test_info3
+-- 아이디(INT, PK, AUTO_INCREMENT), 이름(5), 주소(20), 성별(2), 나이(INT)
+-- 데이터 10개 추가 하기
+CREATE TABLE test_info3 (
+	user_id 	INT AUTO_INCREMENT PRIMARY KEY,
+	user_name 	CHAR(5),
+	addr 		CHAR(20),
+	gender 		CHAR(2),
+	age 		INT	
+);
+
+INSERT INTO test_info3 VALUES (NULL, '원이', '서울 강동구', '여자', 22);
+INSERT INTO test_info3 VALUES (NULL, '미나미', '서울 강서구', '여자', 21);
+INSERT INTO test_info3 VALUES (NULL, '리브', '서울 강남구', '여자', 21);
+INSERT INTO test_info3 VALUES (NULL, '메이', '서울 강동구', '여자', 19);
+INSERT INTO test_info3 VALUES (NULL, '제나', '서울 용산구', '여자', 19);
+INSERT INTO test_info3 (user_name, addr, gender, age) VALUES ('권지용', '경기 수원시', '남자', 40);
+INSERT INTO test_info3 (user_name, addr, gender, age) VALUES ('동영배', '경기 평택시', '남자', 41);
+INSERT INTO test_info3 (user_name, addr, gender, age) VALUES ('강대성', '경기 화성시', '남자', 38);
+INSERT INTO test_info3 (user_name, addr, gender, age) VALUES ('박지훈', '경기 이천시', '남자', 26);
+INSERT INTO test_info3 (user_name, addr, gender, age) VALUES ('최우식', '경기 양주시', '남자', 38);
+
+SELECT * FROM test_info3;
