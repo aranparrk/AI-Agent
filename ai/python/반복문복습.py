748d6174cb0d180e5a4ee2c9e81fa8960abeb3a9
aranparrk <aranparrk@gmail.com>
2026-09-11 11:03:13 +0900

    continue 문제

--- ai/python/반복문복습.py
+++ ai/python/반복문복습.py
@@ -86,3 +86,19 @@ for i in range(star):
     for j in range(star - i):
         print('*', end=' ')
     print()
+
+#%%
+# continue : 반복문에서 아래의 문장을 수행하지 않고 반복문으로 이동
+
+n = int(input('정수 입력 : '))
+for i in range(1, n + 1):
+    if i % 2 == 0: continue
+    print(i)
+
+#%%
+# continue문을 이용해 3과 5의 배수를 제외하고 출력하기
+
+n = int(input('정수 입력 : '))
+for i in range(1, n + 1):
+    if i % 3 == 0 or i % 5 == 0: continue
+    print(i)
