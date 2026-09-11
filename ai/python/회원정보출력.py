71a2bea8ac299a21d04800e5d675ba8a35ed3e54
aranparrk <aranparrk@gmail.com>
2026-09-11 12:30:37 +0900

    반복문 연습문제 추가

--- ai/python/회원정보출력.py
+++ ai/python/회원정보출력.py
@@ -47,3 +47,33 @@ print(f'이름 : {name}')
 print(f'나이 : {age}')
 print(f'성별 : {gender_name}')
 print(f'직업 : {job_name}')
+
+#%%
+# 짝수 / 홀수 개수 세기
+# 정수를 하나씩 계속 입력받다가, -1이 입력되면 반복을 종료합니다. 그동안 입력받은 숫자 중 짝수의 개수와 홀수의 개수를 각각 출력하세요.
+# while과 break 사용
+even = 0
+odd = 0
+
+while True:
+    num = int(input('정수를 입력하세요 : '))
+
+    if num == -1:
+        break
+
+    if num % 2 == 0:
+        even += 1
+    else:
+        odd += 1
+
+print(f'짝수 개수 : {even}, 홀수 개수 : {odd}')
+
+#%%
+# 구구단 중 특정 단만 출력하기
+# 2~9 사이의 정수를 입력하세요: 3
+
+dan = int(input('단을 입력하세요. : '))
+
+print(f'{dan}단 시작!')
+for i in range(1, 10):
+    print(f'{dan} X {i} = {dan * i}')
