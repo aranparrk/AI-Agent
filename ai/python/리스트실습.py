a59bfe7afb73168e94b7e4cf3803d0ab74fad972
aranparrk <aranparrk@gmail.com>
2026-09-14 17:15:15 +0900

    ✨ add: 리스트 실습

--- ai/python/리스트실습.py
+++ ai/python/리스트실습.py
@@ -7,3 +7,28 @@ price = list(map(int, input('가격을 입력하세요. : ').split()))
 set_price = min(price[:3]) + min(price[3:]) - 50
 
 print(f'햄버거 세트 가격은 {set_price}원 입니다.')
+
+#%%
+# 리스트 순회하기 : 5대의 자동차 이름을 입력 받음
+# - 범위기반 for문으로 순회해서 출력 : for i in range()
+# - 시퀀스 for문으로 순회해서 출력 for e in 시퀀스
+# - 오름차순, 내림차순 출력
+
+
+cars = list(input('자동차 5대를 입력하세요 : ').split())
+
+# 범위 기반 for문: 오름차순
+ascending_cars = sorted(cars)
+
+print('오름차순 : ', end='')
+for i in range(len(ascending_cars)):
+    print(ascending_cars[i], end=' ')
+
+print()
+
+# 시퀀스 for문: 내림차순
+descending_cars = sorted(cars, reverse=True)
+
+print('내림차순 : ', end='')
+for car in descending_cars:
+    print(car, end=' ')
